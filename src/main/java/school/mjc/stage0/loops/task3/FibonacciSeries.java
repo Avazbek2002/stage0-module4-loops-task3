ca69586ee8d3a8bba04b8274f225bebd49e6a271
Avazbek Isroilov <64166521+Avazbek2002@users.noreply.github.com>
2022-07-31 15:23:23 +0500

    loop

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        for (int i = 0; i <= lastFibonacci; i++) {
+            System.out.println(fibonacci(i));
+        }
+    }
+
+    public int fibonacci (int n) {
+        if (n == 1) return 0;
+        if (n == 2) return 1;
+
+        return fibonacci(n-1) + fibonacci(n-2);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,8 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+        int sum = 0;
+        sum = ((int)Math.pow(10, lengthOfLastNumber + 1) - 10)/9 - lengthOfLastNumber;
+        System.out.println(sum);
     }
 }
